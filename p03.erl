33e5ff74be2bd6738dc80a6d0c7eba320da8af39
Yathansh Tewatia <yathansh@gmail.com>
2026-06-07 15:38:57 +0530

    better types for p03

--- p03.erl
+++ p03.erl
@@ -1,8 +1,9 @@
 -module(p03).
 -export([kth/2]).
 
--spec kth([T], integer()) -> T | {error, out_of_bounds} | {error, empty_list}.
-kth([], _) -> {error, empty_list};
+
+-spec kth([T], integer()) -> T | {error, out_of_bounds}.
+kth([], _) -> {error, out_of_bounds};
 kth([X | _], 1) -> X;
 kth([_ | Arr], Remaining) when Remaining > 1 -> kth(Arr, Remaining - 1);
 kth(_, _) -> {error, out_of_bounds}.
